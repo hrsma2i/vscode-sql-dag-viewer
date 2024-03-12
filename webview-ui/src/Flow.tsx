942b5f1b24e4cfe688c95b6c6c25703f38c83ec7
hrsma2i <hrs.ma2i@gmail.com>
2024-03-12 21:12:40 +0900

    parse SQL

--- webview-ui/src/Flow.tsx
+++ webview-ui/src/Flow.tsx
@@ -1,64 +1,17 @@
 import React, { useCallback } from 'react';
+import { Parser, From } from "node-sql-parser";
 import ReactFlow, {
   ReactFlowProvider,
-  Panel,
   useNodesState,
   useEdgesState,
   useReactFlow,
+  Node,
+  Edge,
 } from 'reactflow';
 import 'reactflow/dist/style.css';
 import ELK from 'elkjs/lib/elk.bundled.js';
 
 const elk = new ELK();
-
-export const initialNodes = [
-  {
-    id: '1',
-    type: 'input',
-    data: { label: 'input' },
-    position: { x: 0, y: 0 },
-  },
-  {
-    id: '2',
-    data: { label: 'node 2' },
-    position: { x: 0, y: 100 },
-  },
-  {
-    id: '2a',
-    data: { label: 'node 2a' },
-    position: { x: 0, y: 200 },
-  },
-  {
-    id: '2b',
-    data: { label: 'node 2b' },
-    position: { x: 0, y: 300 },
-  },
-  {
-    id: '2c',
-    data: { label: 'node 2c' },
-    position: { x: 0, y: 400 },
-  },
-  {
-    id: '2d',
-    data: { label: 'node 2d' },
-    position: { x: 0, y: 500 },
-  },
-  {
-    id: '3',
-    data: { label: 'node 3' },
-    position: { x: 200, y: 100 },
-  },
-];
-
-export const initialEdges = [
-  { id: 'e12', source: '1', target: '2', animated: true },
-  { id: 'e13', source: '1', target: '3', animated: true },
-  { id: 'e22a', source: '2', target: '2a', animated: true },
-  { id: 'e22b', source: '2', target: '2b', animated: true },
-  { id: 'e22c', source: '2', target: '2c', animated: true },
-  { id: 'e2c2d', source: '2c', target: '2d', animated: true },
-];
-
 
 const useLayoutedElements = () => {
   const { getNodes, setNodes, getEdges, fitView } = useReactFlow();
@@ -98,10 +51,85 @@ const useLayoutedElements = () => {
   return { getLayoutedElements };
 };
 
+
+type With = {
+  name: {
+    value: string
+  };
+  stmt: {
+    ast: Select
+  };
+}
+
+type Select = {
+  with: Array<With> | null;
+  from: Array<From> | null;
+}
+
 const LayoutFlow = () => {
-  const [nodes, , onNodesChange] = useNodesState(initialNodes);
-  const [edges, , onEdgesChange] = useEdgesState(initialEdges);
+  const parser = new Parser()
+  const sql = `
+    with
+      b as (
+        select *
+        from a
+      )
+      , c as (
+        select *
+        from b
+      )
+      , e as (
+        select *
+        from d
+      )
+
+    select *
+    from c
+      join e
+        on c.id = e.id
+  `
+
+  const ast = parser.astify(sql) as Select
+
+  const initNodes: Node[] = []
+  const initEdges: Edge[] = []
+
+  ast.with?.forEach((cte) => {
+    const child = cte.name.value
+    const childNode = { id: child, data: { label: child }, position: { x: 0, y: 0 } }
+    if (!initNodes.includes(childNode)) {
+      initNodes.push(childNode)
+    }
+
+    cte.stmt.ast.from?.forEach((f) => {
+      const parent = f.table!
+      const parentNode = { id: parent, data: { label: parent }, position: { x: 0, y: 0 } }
+      if (!initNodes.includes(parentNode)) {
+        initNodes.push(parentNode)
+      }
+      initEdges.push({ id: `${parent}->${child}`, source: parent, target: child, animated: true })
+    })
+  })
+
+  const mainName = '(main)'
+  initNodes.push({ id: mainName, data: { label: mainName }, position: { x: 0, y: 0 } })
+
+  ast.from?.forEach((f) => {
+      const parent = f.table!
+      const parentNode = { id: parent, data: { label: parent }, position: { x: 0, y: 0 } }
+      if (!initNodes.includes(parentNode)) {
+        initNodes.push(parentNode)
+      }
+      initEdges.push({ id: `${parent}->${mainName}`, source: parent, target: mainName, animated: true })
+  })
+
+  const [nodes, , onNodesChange] = useNodesState(initNodes);
+  const [edges, , onEdgesChange] = useEdgesState(initEdges);
   const { getLayoutedElements } = useLayoutedElements();
+
+  React.useEffect(() => {
+    getLayoutedElements({ 'elk.algorithm': 'layered', 'elk.direction': 'DOWN' })
+  }, []);
 
   return (
     <div style={{ height: '100vh', width: '100vw' }}>
@@ -112,40 +140,6 @@ const LayoutFlow = () => {
         onEdgesChange={onEdgesChange}
         fitView
       >
-        <Panel position="top-right">
-          <button
-            onClick={() =>
-              getLayoutedElements({ 'elk.algorithm': 'layered', 'elk.direction': 'DOWN' })
-            }
-          >
-            vertical layout
-          </button>
-          <button
-            onClick={() =>
-              getLayoutedElements({ 'elk.algorithm': 'layered', 'elk.direction': 'RIGHT' })
-            }
-          >
-            horizontal layout
-          </button>
-          <button
-            onClick={() =>
-              getLayoutedElements({
-                'elk.algorithm': 'org.eclipse.elk.radial',
-              })
-            }
-          >
-            radial layout
-          </button>
-          <button
-            onClick={() =>
-              getLayoutedElements({
-                'elk.algorithm': 'org.eclipse.elk.force',
-              })
-            }
-          >
-            force layout
-          </button>
-        </Panel>
       </ReactFlow>
     </div>
   );
